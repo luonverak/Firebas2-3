633780e800b1ccafb88a6d498f1aa937160accc5
luonverak <112792376+luonverak@users.noreply.github.com>
2024-02-21 15:15:16 +0700

    code commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:demo_firebase2_3/auth/controller/auth_controller.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'auth/view/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
